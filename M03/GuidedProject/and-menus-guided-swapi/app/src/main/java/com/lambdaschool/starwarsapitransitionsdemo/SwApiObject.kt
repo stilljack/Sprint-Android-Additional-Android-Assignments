369bcab4f6885b62a5d456aab202be1271ee65d0
jack seymour <stilljack@gmail.com>
2019-10-09 12:12:04 -0700

    init, guided swapi complete

---
+++ M03/GuidedProject/and-menus-guided-swapi/app/src/main/java/com/lambdaschool/starwarsapitransitionsdemo/SwApiObject.kt
@@ -0,0 +1,28 @@
+package com.lambdaschool.starwarsapitransitionsdemo
+
+import java.io.Serializable
+
+abstract class SwApiObject(id: Int, name: String) : Serializable {
+    var id: Int = 0
+        protected set
+    var category: String? = null
+    var name: String
+        protected set
+
+    init {
+        this.id = id
+        this.name = name
+    }
+
+    abstract override fun toString(): String
+
+    companion object {
+        // TODO 8b: Move comparison to the object
+        fun compareNames (o1:SwApiObject, o2:SwApiObject) {
+            o1.name.compareTo(o2.name)
+
+        }
+
+
+    }
+}
