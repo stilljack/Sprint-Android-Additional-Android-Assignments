eedd036f8163a42ec10b63ecd0bec943230fdc58
jack seymour <stilljack@gmail.com>
2019-10-09 15:39:29 -0700

    we're gonna call it functional for now so i can get back to working on map stuff but tip top todo is figure out why it's got the nav view hamburer on a second appbar

--- M03/Assignment/app/src/main/java/com/saucefan/stuff/menupolish/Menu.kt
+++ M03/Assignment/app/src/main/java/com/saucefan/stuff/menupolish/Menu.kt
@@ -1,23 +1,16 @@
 package com.saucefan.stuff.menupolish
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.view.Menu
-import androidx.appcompat.widget.Toolbar
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main2.*
+import kotlinx.android.synthetic.main.content_main2.*
 
 
 class Menu : AppCompatActivity() {
@@ -31,16 +24,60 @@ class Menu : AppCompatActivity() {
 
 
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-    
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener {menuItem->
+            menuItem.isChecked = true
+            Toast.makeText(baseContext,"${menuItem.title}", Toast.LENGTH_SHORT).show()
+            when (menuItem.itemId) {
+                R.id.nav_menu -> {
+                    tv.text="main menu"
+                    tv.setBackgroundColor(Color.WHITE)
+                }
+                R.id.nav_difficulty -> {
+                    tv.text="difficulty selector"
+                    tv.setBackgroundColor(Color.RED)
+                }
+                R.id.nav_gfx_swap -> {
+                    tv.text="gfxswap"
+                    tv.setBackgroundColor(Color.BLUE)
+                }
+
+            }
+            drawerLayout.closeDrawers()
+            drawerLayout.invalidate()
+            true
+        }
 
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main2, menu)
+        menuInflater.inflate(R.menu.sort_menu, menu)
+
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item?.itemId) {
+            R.id.menu_nightmode -> {
+                Toast.makeText(this, "we'd swap over to spooky nightmode (GHOST AND GHOULS ONLY)", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_score_display -> {
+                Toast.makeText(this, "we'd turn on scores here", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item as MenuItem)
+    }
+
 }
 
 
