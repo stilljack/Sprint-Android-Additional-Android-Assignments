12762c4296b88c3a053d3728b9e5c59b2a3cc162
jack seymour <stilljack@gmail.com>
2019-10-10 12:07:09 -0700

    lets implement an algo to draw an arbitrary line!

--- M02/app/src/main/java/com/saucefan/stuff/mapassign/MapDraw.kt
+++ M02/app/src/main/java/com/saucefan/stuff/mapassign/MapDraw.kt
@@ -12,9 +12,70 @@ class MapDraw(var mMap: GoogleMap) {
     val sydney = LatLng(-34.0, 151.0)
 
 
+//Bresenham’s Algorithm
+
+    fun bresenhamAlgo(x1:Double,y1: Double, x2: Double, y2:Double) {
+
+        //iterators
+        var x:Double
+        var y:Double
+        var dx:Double
+        var dy:Double
+        var dx1:Double
+        var dy1:Double
+        var px:Double
+        var py:Double
+        var  xe:Double
+        var ye:Double
+        var i:Double
+
+        //deltas, difference of end points on both axises
+        dx=x2-x1
+        dy=y2-y1
+
+        //"positive copy" of deltas for iterating, i.e. if negative get absolute value from zero
+        //on number line
+        dx1=Math.abs(dx)
+        dy1=Math.abs(dy)
+
+        //'Calculate error intervals for both axis'
+        //"An error interval is the range of values that a number could have taken before being
+        // rounded or truncated. Error intervals are usually written as a range using inequalities,
+        // with a lower bound and an upper bound."
+        //this means very little to me -Jack
+        px = 2 *dy1-dx1
+        py = 2 *dx1 -dy1
+
+        //if the line is x-axis dominant, i.e.
+            //ie if it goes further in the x axis then in the why
+        if (dy1<=dx1) {
+
+            //if line the draws left ot right
+            if(dx>=0) {
+                x=x1
+                y=y1
+                xe=x2
+            } else{
+                //line is right to left
+                x=x2
+                y=y2
+                xe=x1
+            }
+          //here we call our pixel
+
+
+        }
 
 
 
+    }
+
+
+
+    fun xAxisDomLine(x,y) {
+
+        
+    }
     //draws a cross
     fun example() {
         for (i in -25 until 50){
