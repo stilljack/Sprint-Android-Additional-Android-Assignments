8fa5743faa717e1e8c7486b91395348d8057d995
jack seymour <stilljack@gmail.com>
2019-10-10 12:34:28 -0700

    ok lets see if this works

--- M02/app/src/main/java/com/saucefan/stuff/mapassign/MapDraw.kt
+++ M02/app/src/main/java/com/saucefan/stuff/mapassign/MapDraw.kt
@@ -6,10 +6,6 @@ import com.google.android.gms.maps.model.MarkerOptions
 import kotlin.random.Random
 
 
-
-
-
-
 class MapDraw(var mMap: GoogleMap) {
     val r = Random
     // Add a marker in Sydney and move the camera
@@ -18,78 +14,75 @@ class MapDraw(var mMap: GoogleMap) {
 
 //Bresenham’s Algorithm
 
-    fun bresenhamAlgo(x1:Double,y1: Double, x2: Double, y2:Double) {
+    fun bresenhamAlgo(x1: Double, y1: Double, x2: Double, y2: Double) {
 
         //iterators
-        var x:Double
-        var y:Double
-        var dx:Double
-        var dy:Double
-        var dx1:Double
-        var dy1:Double
-        var px:Double
-        var py:Double
-        var  xe:Double
-        var ye:Double
-        var i:Double
+        var x: Double
+        var y: Double
+        var dx: Double
+        var dy: Double
+        var dx1: Double
+        var dy1: Double
+        var px: Double
+        var py: Double
+        var xe: Double
+        var ye: Double
+        var i: Double
 
         //deltas, difference of end points on both axises
-        dx=x2-x1
-        dy=y2-y1
+        dx = x2 - x1
+        dy = y2 - y1
 
         //"positive copy" of deltas for iterating, i.e. if negative get absolute value from zero
         //on number line
-        dx1=Math.abs(dx)
-        dy1=Math.abs(dy)
+        dx1 = Math.abs(dx)
+        dy1 = Math.abs(dy)
 
         //'Calculate error intervals for both axis'
         //"An error interval is the range of values that a number could have taken before being
         // rounded or truncated. Error intervals are usually written as a range using inequalities,
         // with a lower bound and an upper bound."
         //this means very little to me -Jack
-        px = 2 *dy1-dx1
-        py = 2 *dx1 -dy1
+        px = 2 * dy1 - dx1
+        py = 2 * dx1 - dy1
 
         //if the line is x-axis dominant, i.e.
-            //ie if it goes further in the x axis then in the why
-        if (dy1<=dx1) {
+        //ie if it goes further in the x axis then in the why
+        if (dy1 <= dx1) {
 
             //if line the draws left ot right
-            if(dx>=0) {
-                x=x1
-                y=y1
-                xe=x2
-            } else{
+            if (dx >= 0) {
+                x = x1
+                y = y1
+                xe = x2
+            } else {
                 //line is right to left
-                x=x2
-                y=y2
-                xe=x1
+                x = x2
+                y = y2
+                xe = x1
             }
-          //here we call our pixel
-            xAxisDomLine(x,xe,)
-
+            //here we call our pixel maker
+            xAxisDomLine(x, y, xe, px, dx1, dy1, dx, dy)
         }
-
 
 
     }
 
 
-
     fun xAxisDomLine(
-        x:Double,
-        y:Double,
-                     xe:Double,
-                     px:Double,
-        dx1:Double,
-                     dy1:Double,
-        dx:Double,
-        dy:Double) {
+        x: Double,
+        y: Double,
+        xe: Double,
+        px: Double,
+        dx1: Double,
+        dy1: Double,
+        dx: Double,
+        dy: Double
+    ) {
         makeArbMarker(x, y) // Draw first pixel
-        var i = 0.0
         var x = x
-        var y=y
-        var px=px
+        var y = y
+        var px = px
         while (x < xe) {
             x++
 
@@ -98,36 +91,86 @@ class MapDraw(var mMap: GoogleMap) {
             if (px < 0) {
                 px = px + 2 * dy1
             } else {
-                if (dx < 0 && dy < 0 || dx > 0 && dy > 0) {
-                    y = y + 1
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
+                    y++
                 } else {
-                    y = y - 1
+                    y--
                 }
-                px = px + 2 * (dy1 - dx1)
+                px = (px + 2 * (dy1 - dx1))
             }
 
             // Draw pixel from line span at
             // currently rasterized position
             makeArbMarker(x, y)
-            i++
+
         }
 
+    }
 
+    fun yAxisDomLine(
+        x: Double,
+        y: Double,
+        x2: Double,
+        y2: Double,
+        ye: Double,
+        x1: Double,
+        y1: Double,
+        py:Double,
+        dx1: Double,
+        dy1: Double,
+        dx: Double,
+        dy: Double
+    ) {
+var x =x
+        var y = y
+        var ye =ye
+        var py =py
+
+        // Line is drawn bottom to top
+        if (dy >= 0) {
+            x = x1; y = y1; ye = y2
+        } else { // Line is drawn top to bottom
+            x = x2; y = y2; ye = y1
+        }
+
+        makeArbMarker(x, y); // Draw first pixel
+
+        // Rasterize the line
+        while(y < ye) {
+
+            y++
+
+            // Deal with octants...
+            if (py <= 0) {
+                py = (py + 2 * dx1)
+            } else {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
+                    x++
+                } else {
+                    x--
+                }
+                py = (py + 2 * (dx1 - dy1))
+            }
+
+            // Draw pixel from line span at
+            // currently rasterized position
+            makeArbMarker(x, y)
+        }
 
     }
     //draws a cross
     fun example() {
-        for (i in -25 until 50){
+        for (i in -25 until 50) {
             val lat = 34.0 - (i.toDouble() + r.nextDouble(0.0, 1.9) - r.nextDouble(0.0, 1.9))
             val lng = 151.00
 
-            makeArbMarker(lat,lng)
+            makeArbMarker(lat, lng)
 
         }
-        for (i in -35 until 35){
+        for (i in -35 until 35) {
             val lat = 50.0
-            val lng = 151.00- (i.toDouble() + r.nextDouble(0.0, 1.9) - r.nextDouble(0.0, 1.9))
-            makeArbMarker(lat,lng)
+            val lng = 151.00 - (i.toDouble() + r.nextDouble(0.0, 1.9) - r.nextDouble(0.0, 1.9))
+            makeArbMarker(lat, lng)
 
         }
     }
@@ -135,11 +178,10 @@ class MapDraw(var mMap: GoogleMap) {
     fun makeOblong() {
 
 
-
     }
 
-    fun makeArbMarker(x:Double,y:Double) {
-        val chords = LatLng(x,y)
+    fun makeArbMarker(x: Double, y: Double) {
+        val chords = LatLng(x, y)
         mMap.addMarker(MarkerOptions().position(chords).title("${x}, ${y}"))
 
     }
